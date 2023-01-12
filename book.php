019c09478751d0922b09b291c9c1787cda39c489
abror <avazovabror1996@gmail.com>
2023-01-12 15:00:14 +0500

    end changes

--- book.php
+++ book.php
@@ -39,10 +39,61 @@
         </section>
 
     <!-- header section end -->
-    <div class="haeding" style="background-image: url(img/header-bg-3.png);background-repeat: no-repeat;">
+    <div class="heading" style="background-image: url(img/header-bg-3.png);background-repeat: no-repeat;">
         <h1>book now</h1>
     </div>
  
+    <!-- booking section starts -->
+
+    <section class="booking">
+        <h1 class="heading-title">book your trip!</h1>
+
+        <form action="book_form.php" method="post" class="book-form">
+            <div class="flex">
+                <div class="inputBox">
+                    <span>name: </span>
+                    <input type="text" placeholder="enter your name" name="name" autocomplete="off">
+                </div>
+
+                <div class="inputBox">
+                    <span>email: </span>
+                    <input type="email" placeholder="enter your email" name="email" autocomplete="off">
+                </div>
+
+                <div class="inputBox">
+                    <span>phone: </span>
+                    <input type="number" placeholder="enter your number" name="phone" autocomplete="off">
+                </div>
+
+                <div class="inputBox">
+                    <span>address: </span>
+                    <input type="text" placeholder="enter your address" name="address" autocomplete="off">
+                </div>
+
+                <div class="inputBox">
+                    <span>where to: </span>
+                    <input type="text" placeholder="place you want to visit" name="location" autocomplete="off">
+                </div>
+
+                <div class="inputBox">
+                    <span>how many: </span>
+                    <input type="number" placeholder="number of guests" name="guests" autocomplete="off">
+                </div>
+
+                <div class="inputBox">
+                    <span>arrivals: </span>
+                    <input type="date"  name="arrivals" autocomplete="off">
+                </div>
+
+                <div class="inputBox">
+                    <span>leaving: </span>
+                    <input type="date"  name="leaving" autocomplete="off">
+                </div>
+            </div>
+            <input type="submit" value="submit" class="btn" name="send">
+        </form>
+    </section>
+    <!-- booking section ends -->
 
     <!-- Start footer section -->
     <section class="footer">
